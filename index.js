266c05aff06044beffa84f484fe8c8e8f36b3dbe
Nikita Ambalov <js.python.st@gmail.com>
2024-02-10 09:01:22 +0300

    commit 17.async/await exercise #2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 // Задание #1
 const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 let isLoading = false;
@@ -34,3 +35,36 @@ const createNewPost = async () => {
 };
 
 createNewPost();
+*/
+
+// Задание #2
+const TODOS_URL = "https://jsonplaceholder.typicode.com/todos";
+// const getTodosByIds = (ids) => {
+//   const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+//   Promise.all(requests)
+//     .then((responses) => {
+//       const dataResults = responses.map((data) => data.json());
+//       return Promise.all(dataResults);
+//     })
+//     .then((allTasks) => {
+//       console.log(allTasks);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
+const getTodosByIdsAsync = async (ids) => {
+  try {
+    const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+    const responses = await Promise.all(requests);
+    const dataResults = await responses.map((data) => data.json());
+    const allTasks = await Promise.all(dataResults);
+    console.log(allTasks);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// getTodosByIds([43, 21, 55, 100, 10]);
+getTodosByIdsAsync([43, 21, 55, 100, 10]);
